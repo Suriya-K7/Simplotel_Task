bd25a26543e0fde2c1a8d47d3da6591c178f41d7
Suriya-K7 <usraising@gmail.com>
2023-10-18 08:34:59 +0530

    image optimazation

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.toggle("active");
   document.querySelector(".menu").classList.toggle("active");
 });
+
+const blurDiv = document.querySelectorAll(".blur__load");
+blurDiv.forEach((div) => {
+  const img = div.querySelector("img");
+
+  function loaded() {
+    div.classList.add("loaded");
+  }
+  if (img.complete) {
+    loaded();
+  } else {
+    img.addEventListener("load", loaded);
+  }
+});
